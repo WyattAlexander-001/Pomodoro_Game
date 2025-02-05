81b9c0c8a17c005f5b8e6ac367fdc0761c2b8018
WyattAlexander-001 <wyattbushman117@gmail.com>
2025-02-05 03:47:07 -0500

    I finally like my bg. It's done

--- pomodoro_game/scripts/scroll_controller.gd
+++ pomodoro_game/scripts/scroll_controller.gd
@@ -1,7 +1,8 @@
 extends Node2D
 
 const SPEED = 500
+var axis = 1
 
 func _process(delta: float) -> void:
-	var axis = Input.get_axis("left","right")
+	#var axis = Input.get_axis("left","right")
 	position.x += axis * SPEED * delta
